faba03abf8b59af5b3904ca520b201954bc7f43f
karthick-10 <karthick.r@brimmatech.com>
2024-10-30 10:04:18 +0530

    another branch

--- karthick.java
+++ karthick.java
@@ -2,6 +2,7 @@ public class karthick {
     public static void main(String[] args) {
         System.out.println("Helloo Karthick");
         add();
+        System.out.print("another conflict");
     }
     public static void add() {
         System.out.println("Helloo Karthick 1");
