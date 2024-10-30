e426f2187cfac24a08229775376fd2206068d98e
karthick-10 <karthick.r@brimmatech.com>
2024-10-30 09:54:50 +0530

    new branch

--- karthick.java
+++ karthick.java
@@ -1,5 +1,12 @@
 public class karthick {
     public static void main(String[] args) {
         System.out.println("Helloo Karthick");
+        add();
     }
+    public static void add() {
+        System.out.println("Helloo Karthick 1");
+    }
+
+
+
 }
