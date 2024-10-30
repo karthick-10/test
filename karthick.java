171a4b8787dd8976a1b1be2ff05ec75772880461
karthick-10 <karthick.r@brimmatech.com>
2024-10-30 10:03:13 +0530

    creating conflict

--- karthick.java
+++ karthick.java
@@ -2,6 +2,7 @@ public class karthick {
     public static void main(String[] args) {
         System.out.println("Helloo Karthick");
         add();
+        System.out.println("confilict");
     }
     public static void add() {
         System.out.println("Helloo Karthick 1");
